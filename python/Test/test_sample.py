0ef8858de7c3ae84ee4526872c78f02c07901010
tarunsingh-eng <tarun.xpe@gmail.com>
2026-02-22 22:51:36 +0530

    Test updated

--- python/Test/test_sample.py
+++ python/Test/test_sample.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 
 def test_addition():
     assert 2 + 3 == 5
@@ -24,4 +25,5 @@ def test_google(driver):
 
 def test_mysite(driver):
     driver.get("https://tarunsingh.co.in")
+    WebDriverWait(driver, 10).until(lambda d: d.title !="")
     assert "Tarun Singh" in driver.title 
