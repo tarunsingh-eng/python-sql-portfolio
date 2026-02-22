a43fea5d7e9f38f6788558c1ac5f9dbac1ea97f8
tarunsingh-eng <tarun.xpe@gmail.com>
2026-02-22 23:38:10 +0530

    site scroll and click test

--- python/Test/test_sample.py
+++ python/Test/test_sample.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 def test_addition():
     assert 2 + 3 == 5
@@ -26,4 +28,25 @@ def test_google(driver):
 def test_mysite(driver):
     driver.get("https://tarunsingh.co.in")
     WebDriverWait(driver, 10).until(lambda d: d.title !="")
+    #  driver.set_window_position(50,50)
+    driver.set_window_size(1260,700)
+  
     assert "Tarun Singh" in driver.title 
+
+    driver.execute_script("window.scrollBy(0, document.body.scrollHeight)")
+
+    element = WebDriverWait(driver, 10).until( 
+        EC.presence_of_element_located(
+        (By.CSS_SELECTOR, 'a[href="https://tarunsingh.co.in/courses/"]')))
+    
+
+
+    driver.execute_script("arguments[0].scrollIntoView({block:'center'});", element)
+
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, 'a[href="https://tarunsingh.co.in/courses/"]')
+    ))
+    driver.execute_script("arguments[0].click();", element)
+
+
+    input ("Press Enter to close")
