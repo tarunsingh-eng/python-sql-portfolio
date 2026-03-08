0e60279fc96d9f6f79e86018c46d668195a3dff2
tarunsingh-eng <tarun.xpe@gmail.com>
2026-03-08 17:02:05 +0530

    removed YouTubeDwn programm

--- python/fun_scripts/YouTubeDwn.py
+++
@@ -1,6 +0,0 @@
-import pytube
-
-link = input("https://www.youtube.com/watch?v=wm86VOSf3rc")
-video_download = pytube.YouTube(link)
-video_download.streams.first().download()
-print('Video Downloaded', link)
