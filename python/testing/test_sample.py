6a79a9856fb07d24c01a7dfbb78f4646238a164a
tarunsingh-eng <tarun.xpe@gmail.com>
2026-03-24 08:53:27 +0530

    added dependencies

--- python/testing/test_sample.py
+++ python/testing/test_sample.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 
 def test_addition():
     assert 2 + 3 == 5
@@ -22,7 +23,12 @@ def test_division():
 @pytest.fixture
 
 def driver():
-    driver =webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver =webdriver.Chrome(options=options)
     yield driver
     driver.quit()
 
